3b31a6b7d10cafbe2e31d6434a0abadea9fb3b61
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-09-12 12:46:29 +0200

    Identity(alter users tbl)

--- HotelListing.Models/AppUser.cs
+++ HotelListing.Models/AppUser.cs
@@ -4,7 +4,7 @@ namespace HotelListing.Models;
 
 public class AppUser : IdentityUser
 {
-    public string FirsName { get; set; } = null!;
+    public string? FirsName { get; set; } = null!;
 
-    public string LastName { get; set; } = null!;
+    public string? LastName { get; set; } = null!;
 }
---
+++ HotelListing/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HotelListing.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListing.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
+    private readonly IMapper _mapper;
+    private readonly ILogger<AccountController> _logger;
+
+    public AccountController(UserManager<AppUser>
+            userManager, SignInManager<AppUser> signInManager,
+        IMapper mapper, ILogger<AccountController> logger)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _mapper = mapper;
+        _logger = logger;
+    }
+    
+    
+}
