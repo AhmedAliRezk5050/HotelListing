8c85479213328903fdf7383457dba982257c93ab
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-09-15 01:42:25 +0200

    CountryController(create country)

--- HotelListing/Controllers/CountryController.cs
+++ HotelListing/Controllers/CountryController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HotelListing.DataAccess.IRepository;
+using HotelListing.Models;
 using HotelListing.Models.DTOs.Country;
 using HotelListing.Utility;
 using Microsoft.AspNetCore.Authorization;
@@ -48,27 +49,53 @@ public class CountryController : ControllerBase
     }
 
     // GET: api/Countries/5
-    [HttpGet("{id}")]
-    [Authorize]
+    [HttpGet("{id:int}", Name = "GetCountry")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetCountry(int id)
     {
         try
         {
-            var country = await _unitOfWork.Countries.GetAsync(c => c.Id == id, new List<string>{"Hotels"});
-            CountryDto countryDto= _mapper.Map<CountryDto>(country);
+            var country = await _unitOfWork.Countries.GetAsync(c => c.Id == id, new List<string> { "Hotels" });
+            CountryDto countryDto = _mapper.Map<CountryDto>(country);
 
             if (countryDto is null)
             {
                 return NotFound();
             }
+
             return Ok(country);
         }
         catch (Exception e)
         {
             _logger.LogError(e, $"Error in [{nameof(CountryController)}] controller" +
                                 $" and [{nameof(GetCountries)}] Action");
+            return StatusCode(500, "Internal server error. Try again later");
+        }
+    }
+
+    // POST: api/Countries
+    [HttpPost]
+    [Authorize(Roles = AppRoles.Admin)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> CreateCountry(CreateCountryDto model)
+    {
+        try
+        {
+            Country country = _mapper.Map<Country>(model);
+
+            await _unitOfWork.Countries.Add(country);
+
+            await _unitOfWork.SaveAsync();
+
+            return CreatedAtRoute(nameof(GetCountry), new { id = country.Id }, country);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, $"Error in [{nameof(CountryController)}] controller" +
+                                $" and [{nameof(CreateCountry)}] Action");
             return StatusCode(500, "Internal server error. Try again later");
         }
     }
